af79dbbc0f735a5e7f4bc8107562c379f4199a9a
XxDxnixX <38918616+XxDxnixX@users.noreply.github.com>
2019-10-19 13:08:14 +0200

    Original Thanos 1.01

---
+++ src/main/java/com/dxni/mod1/mod1.java
@@ -0,0 +1,16 @@
+package com.dxni.mod1;
+
+        import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(value = mod1.MODID)
+public class mod1{
+
+    public static final String MODID = "mod1";
+
+    public mod1() {
+
+    }
+
+
+}
